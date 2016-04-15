bd5a7a6df32375e974e06c3ba19b1f2499778231
Girija <gshbehera@gmail.com>
2016-04-15 16:47:20 +0530

    first time loggin links added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,18 @@
 class DashboardController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@posts = Post.all.order("created_at DESC")
+		@user_posts = @user.posts
+		@user.followed_users.each do |followed_user|
+			@followed_user_posts = followed_user.posts
+			#puts @followed_user_posts
+			@user_posts = @followed_user_posts + @user_posts
+		end
+		puts @user_posts
+		@posts = @user_posts.reverse
 	end
+		#puts @posts
+		#@posts = posts.all.order("created_at DESC")
+		#end
 	def show_comms
 		@title = "Communities"
 		@user = User.find(params[:id])
